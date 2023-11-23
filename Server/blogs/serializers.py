a586a737926c27198ef9c7b1e1506397715c3684
yunyun <1319849186@qq.com>
2023-11-23 14:35:32 +0800

    “更新代码”

--- Server/blogs/serializers.py
+++ Server/blogs/serializers.py
@@ -13,34 +13,31 @@ class BlogSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'authorId', 'author', 'update_time', 'content']
 
     def validate(self, data):
-        # 多字段的联合唯一性约束
-        if Blog.objects.filter(title=data.get('title'), author=self.context.get('request').user).exists():
+        # 多字段的联合唯一性约束, 排除更新时因为与自己比较而触发唯一性约束
+        if Blog.objects.filter(title=data.get('title'), author=self.context.get('request').user).exclude(
+                pk=getattr(self.instance, 'pk', None)).exists():
             raise serializers.ValidationError({'title': '標題已存在'})
         return data
 
     def create(self, validated_data):
-        author = self.context.get('request').user
-        validated_data['author'] = author  # 作者字段由後端生成
-        instance = super().create(validated_data)  # 創建博客
+        validated_data['author'] = self.context.get('request').user  # 作者字段由後端生成
+        return super().create(validated_data)
 
-        pattern = re.compile(r'<img .*?src="/media/(%s/blog/image/.*?)".*?>' % re.escape(str(author)))
-        images = re.findall(pattern, validated_data.get('content'))  # 匹配所有的圖片
-
-        Image.objects.filter(author=author, blog__isnull=True, image__in=images).update(blog=instance)  # 為使用的圖片設置blog字段
-        Image.objects.filter(author=author, blog__isnull=True).delete()  # 刪除多餘的圖片(上傳後又沒使用)
-
+    def save(self, **kwargs):
+        instance = super().save(**kwargs)  # 調用父類方法
+        self.process_images()  # 處理圖片
         return instance
 
-    def update(self, instance, validated_data):
-        author = instance.author  # 在正則表達式中使用變量, 需要使用%s字符串格式化
-        pattern = re.compile(r'<img .*?src="/media/(%s/blog/image/.*?)".*?>' % re.escape(str(author)))
-        images = re.findall(pattern, validated_data.get('content', instance.content))  # 匹配所有的圖片
+    def process_images(self):
+        """處理圖片"""
+        author = self.instance.author  # 在正則表達式中使用變量, 需要使用%s字符串格式化
+        pattern = re.compile(r'<img .*?src="/media/(%s/blog/image/.*?)".*?>' % re.escape(author.username))
+        images = re.findall(pattern, self.instance.content)  # 匹配所有的圖片
 
-        Image.objects.filter(author=author, blog=instance).exclude(image__in=images).delete()  # 更新被刪除的圖片
-        Image.objects.filter(author=author, blog__isnull=True, image__in=images).update(blog=instance)  # 為使用的圖片設置blog字段
-        Image.objects.filter(author=author, blog__isnull=True).delete()  # 刪除多餘的圖片(上傳後又沒使用)
-
-        return super().update(instance, validated_data)
+        # 刪除被刪除的圖片, 為使用的圖片設置blog字段, 刪除多餘的圖片(上傳後又沒使用)
+        Image.objects.filter(author=author, blog=self.instance).exclude(image__in=images).delete()
+        Image.objects.filter(author=author, blog__isnull=True, image__in=images).update(blog=self.instance)
+        Image.objects.filter(author=author, blog__isnull=True).delete()
 
 
 class ImageSerializer(serializers.ModelSerializer):
